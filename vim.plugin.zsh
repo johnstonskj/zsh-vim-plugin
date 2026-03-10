aa27f25159bf5393936b7f35b1bed8b82b1c767e
Simon Johnston <johnstonskj@gmail.com>
2026-03-10 11:18:07 -0700

    style: new header doc formatting

--- vim.plugin.zsh
+++ vim.plugin.zsh
@@ -1,8 +1,10 @@
 # -*- mode: sh; eval: (sh-set-shell "zsh") -*-
 #
-# Plugin Name: vim
-# Description: Zsh plugin to redirect vi to Vim.
-# Repository: https://github.com/johnstonskj/zsh-vim-plugin
+# @name: vim
+# @brief: Provide alias `vi` for Vim.
+# @repository: https://github.com/johnstonskj/zsh-vim-plugin
+# @version: 0.1.1
+# @license: MIT AND Apache-2.0
 #
 # Public variables:
 #
